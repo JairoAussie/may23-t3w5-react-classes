34750a14ccdf13cf13bfb50f4b580a357e1a26ed
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-11-18 11:30:00 +1000

    React lifecycle methods

--- pokemon-class/src/PokemonDisplay.jsx
+++ pokemon-class/src/PokemonDisplay.jsx
@@ -3,16 +3,25 @@ import React from "react"
 export default class PokemonDisplay extends React.Component{
     constructor(props){
         super(props);
-
+        this.name = "Pikachu"
         //state stores data per component
         this.state = {
             pokemonName: null
         }
     }
+    componentDidMount(){
+        this.name = "Bulbasaur"
+        console.log(this.name);
+        console.log("Did mount triggers after the component was rendered");
+    }
+
+    componentWillUnmount(){
+        console.log("Will unmount triggers after the component is unmounted (unlikely in this app)")
+    }
 
     render(){
         return(
-            <h2>Pokemon name</h2>
+            <h2>Pokemon {this.name}</h2>
         )
     }
     
